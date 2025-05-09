401aa8a9c5dc2a00407389aaf7eaef6d84fe4a73
EnzoRZequim <enzo.zequim@gmail.com>
2025-05-08 21:33:15 -0300

    FilaEmergencia  e botões menu

---
+++ FilaEmergencial.java
@@ -0,0 +1,66 @@
+public class FilaEmergencial {
+    ItemFila[] dados; 
+    int primeiro, ultimo, ocupacao; //Tem como add um dado só para saber que é emergencial quando visualizar a fila ?
+
+    public FilaEmergencial(int capacidade) {
+        primeiro = 0;
+        ultimo = 0;
+        ocupacao = 0;
+        dados = new ItemFila[capacidade];
+    }
+
+    public FilaEmergencial() {
+        this(10);
+    }
+
+    public boolean filaVazia() {
+        return ocupacao == 0;
+    }
+
+    public boolean filaCheia() {
+        return ocupacao == dados.length;
+    }
+
+    private int proximaPosicao(int posicao) {
+        return (posicao + 1) % dados.length;
+    }
+
+    public void enfileira(ItemFila item) { 
+        if (filaCheia())
+            throw new RuntimeException("falha na insercao");
+        dados[ultimo] = item;
+        ultimo = proximaPosicao(ultimo);
+        ocupacao++;
+    }
+
+    public ItemFila desenfileira() { 
+        if (filaVazia())
+            throw new RuntimeException("falha na remocao");
+        ItemFila aux = dados[primeiro];
+        primeiro = proximaPosicao(primeiro);
+        ocupacao--;
+        return aux;
+    }
+
+    @Override
+    public String toString() {
+        if (filaVazia())
+            return "fila vazia";
+        String s = "";
+        for (int i = primeiro, cont = 0; cont < ocupacao; i = proximaPosicao(i), cont++) {
+            s = s + dados[i] + "\n";
+        }
+        return s;
+    }
+
+    public String stringVetor() {
+        String s = "";
+        for (int i = 0; i < dados.length; i++) {
+            if (dados[i] == null)
+                s += "_ ";
+            else
+                s += dados[i] + " ";
+        }
+        return s;
+    }
+}
--- TesteFilaVetor.java
+++ TesteFilaVetor.java
@@ -22,14 +22,16 @@ public class TesteFilaVetor {
         }
 
         FilaDeImpressao filaImpressao = new FilaDeImpressao(tamanho);
+        FilaEmergencial filaEmergencial = new FilaEmergencial(tamanho);
 
         int condisao;
         do {
-            System.out.println("Escolha uma opção: " + "\n");
+            System.out.println("Escolha uma opção (0 fecha o programa): " + "\n");
             System.out.println("1 - empilhar documento");
             System.out.println("2 - empilhar documento EMERGENCIAL");
             System.out.println("3 - desenfileirar documento (Doc emergencial desempilha primeiro)");
             System.out.println("4 - mostrar fila");
+            System.out.println("5 - Capacidade da fila");
             condisao = sc.nextInt();
 
             if (condisao == 1) {
@@ -49,15 +51,44 @@ public class TesteFilaVetor {
                     System.out.println("hora: " + horaFormatada + " - arquivo: " + nomeArquivo + " - pessoa: " + nomePessoa +"\n");
                 }
             } else if (condisao == 2) {
+                if (filaEmergencial.filaCheia()){
+                    System.out.println("Fila esta cheia.\n");
+                }else {
+                    System.out.println("Digite o nome do arquivo:");
+                    String nomeArquivo = sc.next();
+                    System.out.println("Digite o nome da pessoa:");
+                    String nomePessoa = sc.next();
+
+                    LocalTime hora = LocalTime.now();
+                    LocalTime horaFormatada = LocalTime.of(hora.getHour(), hora.getMinute(), hora.getSecond());
+
+                    filaImpressao.enfileira(new ItemFila(nomeArquivo, nomePessoa, horaFormatada));
+
+                    System.out.println("hora: " + horaFormatada + " - arquivo: " + nomeArquivo + " - pessoa: " + nomePessoa + "\n");
+                }
 
             } else if (condisao == 3) {
-                
+                if (filaEmergencial.ocupacao != 0) 
+                    filaEmergencial.desenfileira();                    
+                else if (filaImpressao.ocupacao != 0)
+                    filaImpressao.desenfileira();
+                else
+                    System.out.println("Não tem documentos na fila");
+
             } else if (condisao == 4) {
-                System.out.println(filaImpressao);
+                if (filaEmergencial.ocupacao != 0)
+                    System.out.println("Fila Emergencial: " + filaEmergencial);
+                System.out.println("Fila Emergencial vazia");
+                if (filaImpressao.ocupacao != 0)   
+                    System.out.println("Fila de Impressao" + filaImpressao);
+                System.out.println("Fila de impressao vazia");
+
+            } else if (condisao == 5) {
+                System.out.println("Fila de Impressao: " + filaImpressao.ocupacao + tamanho);
+                System.out.println("Fila Emergencial: " );
 
             } else
                 System.out.println("Opção inválida. Tente novamente.");
-
             
         } while (condisao != 0);
         
